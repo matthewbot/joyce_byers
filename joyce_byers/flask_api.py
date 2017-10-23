4b0ed69fa4121750fbeac26185b467824ecbdb04
Matt Thompson <mthompson2@spacex.com>
2017-10-22 18:42:47 -0700

    Replying to twilio

--- joyce_byers/flask_api.py
+++ joyce_byers/flask_api.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
 
 app = Flask('joyce_byers')
 
-@app.route('/')
+@app.route('/twilio', methods=['GET', 'POST'])
 def hello_world():
-	return 'Hello world'
+    body = request.values.get('Body', None)
+    
+    response = MessagingResponse()
+    response.message("Hello World! You sent: {}".format(body))
+    return str(response)
--- run.py
+++ run.py
@@ -3,4 +3,4 @@
 from joyce_byers.flask_api import app
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='0.0.0.0')
